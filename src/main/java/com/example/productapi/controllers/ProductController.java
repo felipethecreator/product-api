7382c962228f92d3eb5c7414f73f832864b31438
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-07-31 00:07:39 -0300

    feat(product): add product repository

--- src/main/java/com/example/productapi/controllers/ProductController.java
+++ src/main/java/com/example/productapi/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.productapi.controllers;
 
+import com.example.productapi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
     @GetMapping
     public ResponseEntity GetAllProducts() {
-        return ResponseEntity.ok("Hello world");
+        var allProducts = productRepository.findAll();
+        return ResponseEntity.ok(allProducts);
     }
 }
