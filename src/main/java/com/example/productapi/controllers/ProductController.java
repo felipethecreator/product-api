9e3cded3059be273bc3a30418732da3baa7288cd
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-07-31 23:34:18 -0300

    feat(ProductController): add GetById and UpdateById products

--- src/main/java/com/example/productapi/controllers/ProductController.java
+++ src/main/java/com/example/productapi/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.productapi.controllers;
 
+import com.example.productapi.ProductApiApplication;
 import com.example.productapi.dto.RequestProduct;
 import com.example.productapi.model.product.Product;
 import jakarta.validation.Valid;
@@ -7,6 +8,9 @@ import com.example.productapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -19,11 +23,33 @@ public class ProductController {
         return ResponseEntity.ok(allProducts);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity GetProductById(@PathVariable String id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @PostMapping
     public ResponseEntity CreateProduct(@RequestBody @Valid RequestProduct data) {
         Product newProduct = new Product(data);
         System.out.println(data);
         productRepository.save(newProduct);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity UpdateProduct(@PathVariable String id, @RequestBody @Valid RequestProduct data) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(data.name());
+                    existingProduct.setPrice_in_cents(BigDecimal.valueOf(data.price_in_cents()));
+                    Product updatedProduct = productRepository.save(existingProduct);
+                    return ResponseEntity.ok(updatedProduct);
+                })
+                .orElse(ResponseEntity.notFound().build());
+
     }
 }
